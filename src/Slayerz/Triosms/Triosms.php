651e2c00abfa67d1d8822526186a38a7c8bf8149
Pikri Mohammad <pikri@oddtech.com.my>
2014-11-01 12:15:32 +0800

    Add balance check function call before send()
    Make sure the balance is not zero before proceed with sending SMS

--- src/Slayerz/Triosms/Triosms.php
+++ src/Slayerz/Triosms/Triosms.php
@@ -13,6 +13,12 @@ class Triosms {
         $this->format = \Config::get('triosms::format');
     }
 
+    /**
+     * Check credit balance for specified account
+     *
+     * @param string $mode : account you wish to check balance from
+     * @return string containing cURL response
+     */
     public function balance($mode = '')
     {
         $curl = new Curl\Curl();
@@ -28,15 +34,32 @@ class Triosms {
 
         if ($curl->error)
         {
-            return $curl->error_code;
+            return false;
         } else
         {
             return $curl->response;
         }
     }
 
+    /**
+     * Send SMS from specified account
+     *
+     * @param string $recipient : recipient mobile number
+     * @param string $message : message to be sent
+     * @param string $mode : account to send the SMS from
+     * @param string $format : content type
+     * @return string containing cURL response
+     */
     public function send($recipient, $message, $mode = '', $format = '')
     {
+        /* make sure credit is available before proceed */
+        $balance = Sms::balance(empty($mode) ? $this->mode : $mode);
+
+        if ($balance == 0)
+        {
+            return false;
+        }
+
         $curl = new Curl\Curl();
         $curl->setopt(CURLOPT_RETURNTRANSFER, true);
 
@@ -54,7 +77,7 @@ class Triosms {
 
         if ($curl->error)
         {
-            return $curl->error_code;
+            return false;
         } else
         {
             return $curl->response;
